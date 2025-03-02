04a1e51b7692996e67b9a3dd686fb15d04a6db88
Arseny Latyshev <arseny.latyshev@gmail.com>
2025-03-02 17:32:24 +0300

    Update Position.kt

--- src/main/java/com/greentree/telegram/queue/model/Position.kt
+++ src/main/java/com/greentree/telegram/queue/model/Position.kt
@@ -4,13 +4,17 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.Id
+import org.hibernate.annotations.OnDelete
+import org.hibernate.annotations.OnDeleteAction
 import java.io.Serializable
 
 @Entity(name = "Position")
 data class Position @JvmOverloads constructor(
 	@Column
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	var queue: Queue? = null,
 	@Column
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	var client: Client? = null,
 	@Column
 	var number: Int = 0,
