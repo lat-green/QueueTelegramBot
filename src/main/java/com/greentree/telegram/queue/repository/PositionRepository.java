3fcd5fb824e845631acc1e9e54266d5023c42f3c
Arseny Latyshev <arseny.latyshev@gmail.com>
2025-03-02 16:46:58 +0300

    Update PositionRepository.java

--- src/main/java/com/greentree/telegram/queue/repository/PositionRepository.java
+++ src/main/java/com/greentree/telegram/queue/repository/PositionRepository.java
@@ -1,11 +1,14 @@
 package com.greentree.telegram.queue.repository;
 
-import com.greentree.telegram.queue.model.Client;
 import com.greentree.telegram.queue.model.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PositionRepository extends JpaRepository<Position, Long> {
+
+    @Query("delete from position where client = ?1")
+    void deleteByClientId(long clientId);
 
 }
